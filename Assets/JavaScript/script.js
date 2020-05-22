76bfc8cea058f1489a62608168478fc2fc57cf02
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-05-22 14:35:29 -0500

    added pseudocode to help organize my thoughts

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,6 +1,9 @@
+// Global empty array variable to store names of cities enter in search input tag.
 var cityName = [];
 var date = "";
 
+/* This function will add a <li> tag item into the weather dashboard UI for each city 
+included in the cityName array.*/
 function createCityList() {
 	$("#cityList").empty();
 	for (var i = 0; i < cityName.length; i++) {
@@ -10,6 +13,8 @@ function createCityList() {
 	}
 }
 
+/* This function will push all cities names that are entered into the search input tag 
+when the user clicked the search icon button.*/
 $("#searchBtn").click(function () {
 	cityName.push($("#cityInput").val());
 	localStorage.setItem("cityName", JSON.stringify(cityName));
