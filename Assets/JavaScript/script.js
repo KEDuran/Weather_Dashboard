b568d12e3c50bda60a11883acabc25363ff98e27
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-05-22 16:42:57 -0500

    added the last city searched logic to save last searched city

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,18 +1,23 @@
 // Global empty array variable to store names of cities enter in search input tag.
 var cityName = [];
 var date = "";
+var lastCity = "";
 
-/* Invoking the keepCities() function here will make sure that all city values populate
+/* Invoking the keepLastCity() function here will make sure that all city values populate
  on the page after a refresh*/
-keepCities();
+keepLastCity();
 
 /* This function will add a <li> tag item into the weather dashboard UI for each city 
-included in the cityName array.*/
+included in the cityName array and help keep track of the last city name value that
+was searched.*/
 function createCityList() {
 	$("#cityList").empty();
 	for (var i = 0; i < cityName.length; i++) {
-		var cityLiItem = $("<li>").text(cityName[i]);
-		cityLiItem.addClass("list-group-item");
+		var cityLiItem = $("<button>").text(cityName[i]);
+		cityLiItem.addClass("btn btn-outline-primary");
+		cityLiItem.click(function () {
+			// here is the where the ajax function for weather data.
+		});
 		$("#cityList").append(cityLiItem.prop("outerHTML"));
 	}
 }
@@ -20,18 +25,20 @@ function createCityList() {
 /* This function will push all cities names that are entered into the search input tag 
 when the user clicked the search icon button.*/
 $("#searchBtn").click(function () {
-	cityName.push($("#cityInput").val());
-	localStorage.setItem("cityName", JSON.stringify(cityName));
+	var cityTextValue = $("#cityInput").val();
+	cityName.push(cityTextValue);
+	lastCity = cityTextValue;
+	localStorage.setItem("lastCity", lastCity);
 	createCityList();
 });
 
-/* This function is parsing out the cityName items that are saved in local storage and reassigning 
-them back to the <li> tags in the weather dashboard UI after  a page refresh. This function is called 
-at the beginning of the this code.*/
-function keepCities() {
-	var storedCityName = JSON.parse(localStorage.getItem("cityName"));
-	if (storedCityName !== null) {
-		cityName = storedCityName;
+/* This function its extracting the city name array from local storage and also extracting the last
+searched city value from local storage. Then, the create*/
+function keepLastCity() {
+	var storedLastCity = localStorage.getItem("lastCity");
+	if (storedLastCity !== null && storedLastCity !== "") {
+		lastCity = storedLastCity;
+		cityName.push(storedLastCity);
 	}
 	createCityList();
 }
