091ef3334c7a9b66846387fe819c93b7c31b2f75
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-28 08:17:52 -0500

    back to normal

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,17 +1,15 @@
-// Require dotenv NPM module to hide API keys
-require("dotenv").config();
 // Declaring global empty array variable to store names of cities enter in search input tag.
 var cityName = [];
 /* Declaring global empty string variable to store the name of the last city that was searched.
 Added this variable to help meet HW acceptance criteria.*/
 var lastCity = "";
 // Declaring global API variabl to hold my Open Weather Map API key.
-var API = process.env.API_KEY;
+var API = "1151188a4f0910fbef13fb3a9bafd06c";
 /* Declaring first getURL global variable for the API source link where most of weather data is pulled.
 A second getURL variable will be set in the makeAjaxCall() function for UV index data.*/
 var queryURL =
 	"https://api.openweathermap.org/data/2.5/forecast?appid=" + API + "&q=";
- 
+
 /* Invoking the keepLastCity() function here to make sure the last searced city value is populated
  after a page refresh or when app is opened.*/
 keepLastCity();
